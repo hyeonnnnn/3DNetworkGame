20ca4166bf310dca581fa0079f25139f599a44b5
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-03-04 10:30:19 +0900

    feat: 곰 스포너

---
+++ Assets/02.Scripts/Bear/BearSpawner.cs
@@ -0,0 +1,42 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class BearSpawner : Spawner
+{
+    public static BearSpawner Instance { get; private set; }
+
+    [Header("Bear Spawn Settings")]
+    [SerializeField] private float _spawnInterval = 60f;
+
+    private float _timer;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        _timer += Time.deltaTime;
+        if (_timer >= _spawnInterval)
+        {
+            _timer = 0f;
+            Spawn();
+        }
+    }
+
+    public override void Spawn()
+    {
+        Vector3 spawnPosition = GetRandomSpawnPoint();
+        PhotonNetwork.Instantiate(_prefabName, spawnPosition, Quaternion.identity);
+    }
+}
